1f50edfbc05a2d455072d1d024ae2c578ad97372
unibreakfast <unibreakfast1@gmail.com>
2019-04-13 19:30:30 +0300

    model sneakpeak added

--- script.js
+++ script.js
@@ -20,3 +20,5 @@ function dbCellChange() {
   db.rows[[...this.parentElement.parentElement.parentElement.children].indexOf(this.parentElement.parentElement)][[...this.parentElement.parentElement.children].indexOf(this.parentElement)] = this.value
   console.log(db.rows)
 }
+
+model.innerHTML = `{\n  headers: [${m.data.headers.reduce((html,header,i,arr)=>`${html}"${header}"${arr.length-i-1?', ':''}`,'')}],\n  rows: [\n${m.data.rows.reduce((html,row)=>`${html}    [${row.reduce((str,cell,i)=>`${str}"${cell}"${row.length-i-1?', ':''}`,'')}],\n`,'')}  ]\n}`
