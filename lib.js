486dea3d301e3211efe626ecdbf262dedb094382
unibreakfast <unibreakfast1@gmail.com>
2019-04-14 14:33:16 +0300

    overflowx hidden

--- lib.js
+++ lib.js
@@ -19,13 +19,13 @@ c = {
   setFetch(path) { this.fetchPath = path },
   setPush(path) { this.pushPath = path },
   setFetchParams(params) { this.fetchParams = params },
-  // fetch(cb, params) {
-  //   if (this.fetchParams && params) params = {...this.fetchParams, ...params}
-  //   else if (this.fetchParams) params = this.fetchParams
-  //   fetch(this.fetchPath+ (params? `${this.fetchPath}?${Object.entries(params)
-  //     .map(([key,value])=>`${key}=${value}`).join('&')}` :''))
-  //       .then(resp=>resp.text())
-  //       .then(cb)
-  // },
+  fetch(cb, params) {
+    if (this.fetchParams && params) params = {...this.fetchParams, ...params}
+    else if (this.fetchParams) params = this.fetchParams
+    fetch(this.fetchPath+ (params? `${this.fetchPath}?${Object.entries(params)
+      .map(([key,value])=>`${key}=${value}`).join('&')}` :''))
+        .then(resp=>resp.text())
+        .then(cb)
+  },
   fetch() { return JSON.stringify(this.db) }
 }
